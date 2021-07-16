f4dcc270a823211f8789e6ccdd938cbe97c1b789
surbhi soni <surbhisoni241@gmail.com>
2021-07-16 17:24:09 +0530

    Updates

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const instance = axios.create();
-instance.CancelToken = axios.CancelToken;
-instance.isCancel = axios.isCancel;
-
-export default axios.create({
-    baseURL: "https://randomuser.me/",
-});
